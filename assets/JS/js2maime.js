f17dcc48ca7509af93269f9b476e0393bbfbe5a0
Cana0930 <comugi35.com@icloud.com>
2024-10-25 09:31:08 +0800

    頑張りました

--- assets/JS/js2maime.js
+++ assets/JS/js2maime.js
@@ -1,43 +1,48 @@
 const ham = $('#js-hamburger');
 const nav = $('#js-nav');
 
-// ハンバーガーメニューをクリックしたら
+//ハンバーガーメニュー
 ham.on('click', function (event) {
-  event.stopPropagation(); // イベントのバブリングを防止
-  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外す
+  event.stopPropagation(); 
+  ham.toggleClass('active'); 
+  nav.toggleClass('active'); 
 });
 
-// ページのどこかをクリックしたとき
+//ページのどこかをクリックしたら消える
 $(document).on('click', function () {
   if (nav.hasClass('active')) {
-    ham.removeClass('active'); // ハンバーガーメニューからactiveクラスを削除
-    nav.removeClass('active'); // ナビゲーションメニューからactiveクラスを削除
+    ham.removeClass('active'); 
+    nav.removeClass('active'); 
   }
 });
 
-
-
+//ヘッダーのリンクをクリックして移動
 $(document).ready(function() {
-  // ヘッダーの高さを変数として設定
-  var headerHeight = $('.header-right').outerHeight() +65; 
+  
+  $('.nav__items a').click(function(event) {
+    event.preventDefault(); 
 
-  // ヘッダー内のリンクをクリックしたときの動作
-  $('.header-list a').click(function(event) {
-    event.preventDefault(); // デフォルトの動作を無効化
+    
+    let targetId = $(this).attr('href');
 
-    // クリックされたリンクのhref属性の値を取得（#details-1など）
-    var targetId = $(this).attr('href');
+    
+    let headerHeight = $('.header__inner').outerHeight(); 
 
-    // 移動先の要素の位置を取得し、ヘッダーの高さを引いた値に移動
-    var targetPosition = $(targetId).offset().top - headerHeight;
+    
+    console.log("ヘッダーの高さ: " + headerHeight);
 
-    // スムーズにスクロールする
+    
+    let targetPosition = $(targetId).offset().top - headerHeight - 65;
+
+    
     $('html, body').animate({
       scrollTop: targetPosition
-    }, 800); // スクロール速度（800ms）
+    }, 800); 
   });
 });
+
+
+//写真のスライド
 
 let slideIndex = 0;
 showSlides();
@@ -45,62 +50,109 @@ showSlides();
 function showSlides() {
   let slides = document.getElementsByClassName("slides");
 
-  // すべてのスライドを非表示にする
+  
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
   slideIndex++;
 
-  // 最後のスライドの次は最初に戻す
+  
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
 
-  // 現在のスライドを表示
+  
   slides[slideIndex - 1].style.display = "block";
 
-  // 画像の切り替え間隔（3秒ごとに切り替え）
+  
   setTimeout(showSlides, 8000);
 }
 
 
+
+//クリックにより隠れている要素が開く
 $(document).ready(function() {
-  // 初期状態でmain-contents以下を非表示にする
+  
   $('#main-contents').hide();
 
-  // title-indexをクリックしたらmain-contentsを表示/非表示する
+ 
   $('#title-index').click(function(e) {
-    e.preventDefault();  // デフォルトのリンク動作を防止
-    $('#main-contents').slideToggle();  // スライドで表示・非表示を切り替える
+    e.preventDefault();  
+    $('#main-contents').slideToggle(); 
   });
-});
 
-$(document).ready(function() {
-  // 全てのdtとddを非表示にする
+  
   $('.list-2 dt, .list-2 dd, .list-3 dt, .list-3 dd, .list-4 dt, .list-4 dd').hide();
 
-  // 各indexのタイトルをクリックしたら該当するdlのdt要素を表示/非表示する
+  
   $('#title-index2').click(function(event) {
     event.preventDefault(); 
-    $('.list-2 dt').slideToggle();  // list-2 の dt 要素を表示/非表示
-    $('.list-2 dd').slideUp();      // list-2 の dd 要素は一旦全て非表示
+    $('.list-2 dt').slideToggle();  
+    $('.list-2 dd').slideUp();      
   });
 
   $('#title-index3').click(function(event) {
     event.preventDefault(); 
-    $('.list-3 dt').slideToggle();  // list-3 の dt 要素を表示/非表示
-    $('.list-3 dd').slideUp();      // list-3 の dd 要素は一旦全て非表示
+    $('.list-3 dt').slideToggle();  
+    $('.list-3 dd').slideUp();   
   });
 
   $('#title-index4').click(function(event) {
     event.preventDefault(); 
-    $('.list-4 dt').slideToggle();  // list-4 の dt 要素を表示/非表示
-    $('.list-4 dd').slideUp();      // list-4 の dd 要素は一旦全て非表示
+    $('.list-4 dt').slideToggle();
+    $('.list-4 dd').slideUp(); 
   });
 
-  // dt要素をクリックしたら、その次のdd要素を表示/非表示する
+ 
   $('.list-2 dt, .list-3 dt, .list-4 dt').click(function() {
-    $(this).nextUntil('dt', 'dd').slideToggle(); // 次のdd要素をスライド表示/非表示
+    $(this).nextUntil('dt', 'dd').slideToggle(); 
+  });
+
+  
+  if (window.location.hash) {
+    let targetId = window.location.hash; 
+    $(targetId).trigger('click'); 
+  }
+});
+
+
+
+//１ページ目のmoreをクリックして、指定の位置に移動
+
+$(document).ready(function() {
+
+  if (window.location.hash) {
+    const headerHeight = $('header').outerHeight();
+    const offset = 50;
+    const targetId = window.location.hash; 
+    const targetPosition = $(targetId).offset().top - (headerHeight + offset); // スクロール位置を計算
+
+  
+    $('html, body').animate({
+      scrollTop: targetPosition
+    }, 600); 
+  }
+});
+
+
+
+
+
+// !!!!!!!!!!!!!!!!!!!!
+$(document).ready(function(){
+
+$(".photoofthis").hide();
+  $("#title-index").on('click', () => {
+    $(".photo_of_this").toggle('slow');
+  });
+  $("#title-index2").on('click', () => {
+      $(".photo_of_this2").toggle('slow');
+  });
+  $("#title-index3").on('click', () => {
+    $(".photo_of_this3").toggle('slow');
+  });
+  $("#title-index4").on('click', () => {
+    $(".photo_of_this4").toggle('slow');
   });
 });
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,61 +3,108 @@ const nav = $('#js-nav');
 
 // ハンバーガーメニューをクリックしたら
 ham.on('click', function (event) {
-  event.stopPropagation(); // イベントのバブリングを防止
-  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外す
+  event.stopPropagation(); 
+  ham.toggleClass('active');
+  nav.toggleClass('active');
 });
 
 // ページのどこかをクリックしたとき
 $(document).on('click', function () {
   if (nav.hasClass('active')) {
-    ham.removeClass('active'); // ハンバーガーメニューからactiveクラスを削除
-    nav.removeClass('active'); // ナビゲーションメニューからactiveクラスを削除
+    ham.removeClass('active'); 
+    nav.removeClass('active'); 
   }
 });
 
+
+//トップのスライド
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
   let slides = document.getElementsByClassName("slides");
 
-  // すべてのスライドを非表示にする
+  
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
   slideIndex++;
 
-  // 最後のスライドの次は最初に戻す
+  
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
 
-  // 現在のスライドを表示
+  
   slides[slideIndex - 1].style.display = "block";
 
-  // 画像の切り替え間隔（3秒ごとに切り替え）
+  
   setTimeout(showSlides, 8000);
 }
 
+
+
+//ヘッダーのリンクをクリックして移動
 $(document).ready(function() {
-  // ヘッダーの高さを変数として設定
-  let headerHeight = $('.header__nav').outerHeight() +45; 
-
-  // ヘッダー内のリンクをクリックしたときの動作
+  
   $('.nav__items a').click(function(event) {
-    event.preventDefault(); // デフォルトの動作を無効化
+    event.preventDefault(); 
 
-    // クリックされたリンクのhref属性の値を取得（#details-1など）
+   
     let targetId = $(this).attr('href');
 
-    // 移動先の要素の位置を取得し、ヘッダーの高さを引いた値に移動
+    
+    let headerHeight = $('.header__inner').outerHeight() + 65;
+
+    
     let targetPosition = $(targetId).offset().top - headerHeight;
 
-    // スムーズにスクロールする
+    
     $('html, body').animate({
       scrollTop: targetPosition
-    }, 800); // スクロール速度（800ms）
+    }, 800); 
+  });
+});
+
+
+
+
+
+$(document).ready(function() {
+  
+  $('.choice a').click(function(event) {
+    event.preventDefault(); 
+
+   
+    let targetId = $(this).attr('href');
+
+    
+    let headerHeight = $('.header__inner').outerHeight() + 20;
+
+    
+    let targetPosition = $(targetId).offset().top - headerHeight;
+
+    
+    $('html, body').animate({
+      scrollTop: targetPosition
+    }, 800); 
+  });
+});
+
+
+
+
+
+//２ページ目の指定位置に移動
+$(document).ready(function() {
+  
+  const headerHeight = $('header').outerHeight();
+  const offset = 50;
+
+  
+  $('a[href="./indx2maime.html#title-index2"]').click(function(e) {
+    e.preventDefault(); 
+    window.location.href = $(this).attr('href'); 
   });
 });
